3bda9b650a94bf5372a903d15d5613c168558526
carolinacuadros <soycaro.990@gmail.com>
2026-07-15 13:45:24 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import {
 } from "lucide-react";
 import { getInstagramThumbnail } from "@/lib/instagram.functions";
 const expocamacolPhoto = "/expocamacol.jpg";
-const retrato = "/carolina-portrait.png";
+const carolinaPortrait = "/carolina-portrait.png";
 
 export const Route = createFileRoute("/")({ component: Index });
 
